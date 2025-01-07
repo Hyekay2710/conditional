ab692bb4a766b72247459338713d9463e6871152
Hyekay2710 <ogbangworjacob02@gmail.com>
2025-01-07 17:36:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let grade = 10;
+
+
+function studentGrade(grade) {
+    if (grade >= 70) {
+        console.log("Distinction");
+    } else if (grade >= 50) {
+        console.log("Pass");
+    }else {
+    console.log("fail");
+}
+}
+
+
+
+studentGrade(grade)
