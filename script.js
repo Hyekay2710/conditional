b17159018a444b726232cb190e6f5619b9b50f49
Hyekay2710 <ogbangworjacob02@gmail.com>
2025-01-11 07:05:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let grade = 10;
 function studentGrade(grade) {
     if (grade >= 70) {
         console.log("Distinction");
-    } else if (grade >= 50) {
+    } else if (grade >= 40) {
         console.log("Pass");
     }else {
     console.log("fail");
